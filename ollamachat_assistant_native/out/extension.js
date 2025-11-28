2a02540bdb6c42291e4a7668e4f9fd246977600f
Kavia AI <production@kavia.ai>
2025-11-28 11:21:32 +0000

    Auto-commit: session cgf2e0a588 - ollama-chat-extension-4791-4800

--- ollamachat_assistant_native/out/extension.js
+++ ollamachat_assistant_native/out/extension.js
@@ -1,3 +1,8 @@
-// Minimal JS extension used as safe fallback for early validation
-exports.activate = function(context){ return { dispose: function(){} }; };
-exports.deactivate = function(){};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.activate = activate;
+exports.deactivate = deactivate;
+// Staged TypeScript source; will be used after deps are installed
+// import * as vscode from 'vscode';
+function activate(context) { return { dispose() { } }; }
+function deactivate() { }
